068d501c59f2a3d3c18db024f987ed21621036a2
Ricardo Rocha <me@rrocha.uk>
2022-08-21 20:40:08 +0100

    * UseEvent improvements to allow data to be passed as arguments;

--- ideasboard/src/composables/UseEvent.ts
+++ ideasboard/src/composables/UseEvent.ts
@@ -1,27 +1,40 @@
 import { Ref, ref, UnwrapRef } from 'vue';
 
-type Events = Ref<UnwrapRef<any[]>>
+type Events = Ref<UnwrapRef<Event[]>>
+
+interface Event {
+  key: string;
+  cb: (...data: any[]) => void;
+}
 
 const eventList: Events = ref([]);
 
 export const useEvent = () => {
 
-  const addEvent = (key: string, cb: () => any) => {
-    const event = {
+  const addEvent = (key: string, cb: (...data: any[]) => void): void => {
+    const event: Event = {
       key: key,
       cb: cb,
     };
-    eventList.value.push(event);
+    if ( eventList.value.length === 0 ) {
+      eventList.value.push(event);
+    } else {
+      eventList.value.map((event: Event) => {
+        if ( event.key === key ) {
+          event.cb = cb;
+        }
+      });
+    }
   };
 
   const removeEvent = (key: string) => {
-    console.log('removing', key);
+    eventList.value.splice(eventList.value.findIndex((e: Event) => e.key === key), 1);
   };
 
-  const dispatch = (key: string) => {
-    eventList.value.forEach((e) => {
+  const dispatch = (key: string, ...data: any[]): void => {
+    eventList.value.forEach((e): void => {
       if ( e.key === key ) {
-        e.cb();
+        e.cb(...data);
       }
     });
   };
